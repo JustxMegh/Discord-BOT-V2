0b10716de4500760a7b02a9e03853872686affbc
JustxMegh <saragaldo20@gmail.com>
2026-06-02 01:52:17 +0200

    Update setbenvenuto.js

--- src/commands/setbenvenuto.js
+++ src/commands/setbenvenuto.js
@@ -1,22 +1,10 @@
-const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require('discord.js');
 const { salvaConfig } = require('../utils/database');
 
 module.exports = {
-    // Questa parte serve al bot per registrare il comando su Discord
-    data: new SlashCommandBuilder()
-        .setName('setbenvenuto')
-        .setDescription('Imposta il canale per i messaggi di benvenuto')
-        .addChannelOption(option =>
-            option.setName('canale')
-                .setDescription('Seleziona il canale di testo')
-                .setRequired(true)
-                .addChannelTypes(ChannelType.GuildText)
-        )
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels), // Solo admin/moderatori
-
+    name: 'setbenvenuto.js',
     async execute(interaction) {
         const canale = interaction.options.getChannel('canale');
-        await salvaConfig('welcome_channel', canale.id);
-        await interaction.reply({ content: `✅ Canale di benvenuto impostato a ${canale}`, ephemeral: true });
+        await salvaConfig('benvenuto_channel', canale.id);
+        await interaction.reply({ content: `✅ Canale benvenuto impostato a ${canale}`, ephemeral: true });
     }
 };
